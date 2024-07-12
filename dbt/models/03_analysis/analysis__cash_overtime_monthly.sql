41e9c7cf8ea230c5193f79a5ee95d38431ad156f
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2024-07-12 18:58:56 +0200

    feat: new cash model

---
+++ dbt/models/03_analysis/analysis__cash_overtime_monthly.sql
@@ -0,0 +1,9 @@
+select
+    date_trunc('month', transaction_date)
+    , sum(round(cast(amount_eur as numeric),2)) as diff
+    -- rolling sum
+    , sum(sum(round(cast(amount_eur as numeric),2))) over (order by date_trunc('month', transaction_date)) as balance
+from {{ ref('transactions') }}
+where account ilike '%giro%'
+group by 1
+
