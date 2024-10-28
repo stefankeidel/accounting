de77618c022675f479d233c94ac82776deec4f17
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2024-10-28 07:26:05 +0100

    feat: add percentage delta to networth

--- dbt/models/03_analysis/analysis__networth_overtime_monthly.sql
+++ dbt/models/03_analysis/analysis__networth_overtime_monthly.sql
@@ -1,7 +1,14 @@
-select 
-    date_trunc('month', transaction_date) as month
-    , round(cast(sum(amount_eur) as numeric), 2) as networth_delta
-    , round(cast(sum(sum(amount_eur)) over (order by date_trunc('month', transaction_date)) as numeric), 2) as networth
-from {{ ref('transactions') }}
-group by 1
-order by 1 asc
+with grouped as (
+    select 
+        date_trunc('month', transaction_date) as month
+        , round(cast(sum(amount_eur) as numeric), 2) as networth_delta
+        , round(cast(sum(sum(amount_eur)) over (order by date_trunc('month', transaction_date)) as numeric), 2) as networth
+    from {{ ref('transactions') }}
+    group by 1
+    order by 1 asc
+)
+
+select
+    *
+    , 100.0 * (networth - lag(networth) over (order by month)) / lag(networth) over (order by month) as percentage_change
+from grouped
--- dbt/models/03_analysis/analysis__networth_overtime_yearly.sql
+++ dbt/models/03_analysis/analysis__networth_overtime_yearly.sql
@@ -1,7 +1,16 @@
-select 
-    date_trunc('year', transaction_date) as year
-    , sum(amount_eur) as networth_delta
-    , sum(sum(amount_eur)) over (order by date_trunc('year', transaction_date)) as networth
-from {{ ref('transactions') }}
-group by 1
-order by 1 asc
+with grouped as (
+
+    select 
+        date_trunc('year', transaction_date) as year
+        , sum(amount_eur) as networth_delta
+        , sum(sum(amount_eur)) over (order by date_trunc('year', transaction_date)) as networth
+    from {{ ref('transactions') }}
+    group by 1
+    order by 1 asc
+
+)
+
+select
+    *
+    , 100.0 * (networth - lag(networth) over (order by year)) / lag(networth) over (order by year) as percentage_change
+from grouped
