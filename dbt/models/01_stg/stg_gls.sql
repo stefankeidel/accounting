929c452e7f682afd679c0b43b7300063a0c1438d
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2025-01-14 18:33:07 +0100

    data engineering, bro

--- dbt/models/01_stg/stg_gls.sql
+++ dbt/models/01_stg/stg_gls.sql
@@ -1,18 +1,23 @@
-with src_key as (
+with
+
+valutadatum_replaced as (
+    select
+        replace(valutadatum, '30.02.', '29.02.') as valutadatum_replaced
+        , *
+    from {{ source('public', 'gls') }}
+)
+
+, src_key as (
     select
         *
-        , {{ dbt_utils.generate_surrogate_key(['buchungstag', 'valutadatum', 'name_zahlungsbeteiligter', 'buchungstext', 'betrag', 'saldo_nach_buchung']) }} as transaction_id
-    from {{ source('public', 'gls') }}
+        , {{ dbt_utils.generate_surrogate_key(['valutadatum_replaced', 'name_zahlungsbeteiligter', 'buchungstext', 'betrag', 'saldo_nach_buchung']) }} as transaction_id
+    from valutadatum_replaced
 )
 
 , src as (
     select
         *
-        , case when substring(valutadatum, 1, 5) = '30.02' then
-            to_date(replace(valutadatum, '30.02.', '28.02.'), 'DD.MM.YYYY')
-          else
-            to_date(valutadatum, 'DD.MM.YYYY')
-          end as value_date
+        , to_date(valutadatum_replaced, 'DD.MM.YYYY') as value_date
         , cast(replace(betrag, ',', '.') as float) as amount_eur
         , row_number() over (partition by transaction_id order by load_time desc) as rn
     from src_key
@@ -22,3 +27,6 @@ with src_key as (
 select *
 from src
 where rn = 1
+-- it's called Data Engineering, bro! Found a transaction where the
+-- buchungstext changed between two imports of the same thing. messed up everything, lol
+and transaction_id not in ('6e43801c2628fe73afdf8881f0280b76')
