0aefe4a8b30c610b2784f56e6d04f70aecfd7320
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2025-01-14 22:04:52 +0100

    broken

--- dbt/models/02_mrt/all_depot_transactions_by_category.sql
+++ dbt/models/02_mrt/all_depot_transactions_by_category.sql
@@ -1,21 +1,34 @@
-select
-   load_time as snapshot_date
-   , class
-   , type
-   , region
-   , sum(price_eur) as total_value
-from {{ ref('gls_depot_assets') }}
-where class is not null
-{{ dbt_utils.group_by(n=4) }}
+with all_asset_tx as (
+   select
+     load_time as snapshot_date
+      , date_trunc('month', load_time) as snapshot_month
+      , wkn as id
+      , 'gls' as source
+      , class
+      , type
+      , region
+      , price_eur
+   from {{ ref('gls_depot_assets') }}
+   where class is not null
 
-union all
+   union all
+
+   select
+       transaction_date as snapshot_date
+       , date_trunc('month', transaction_date) as snapshot_month
+       , isin as id
+       , 'traderepublic'as source
+       , class
+       , type
+       , region
+       , amount_eur as price_eur
+   from {{ ref('traderepublic_depot_assets') }}
+   where class is not null
+)
 
 select
-    transaction_date as snapshot_date
-    , class
-    , type
-    , region
-    , sum(amount_eur) as total_value
-from {{ ref('traderepublic_depot_assets') }}
-where class is not null
-{{ dbt_utils.group_by(n=4) }}
+  *
+  , row_number() over (partition by id, source, month order by snapshot_date desc) as rn
+from all_asset_tx
+
+-- todo: join against a list of months and always take what is closest to the month or the latest in that month
