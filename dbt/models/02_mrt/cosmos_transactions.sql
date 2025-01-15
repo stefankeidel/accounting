9bb43b748e415699bd79899a0446610b62955118
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2025-01-15 11:21:51 +0100

    +sqlfmt

--- dbt/models/02_mrt/cosmos_transactions.sql
+++ dbt/models/02_mrt/cosmos_transactions.sql
@@ -1,41 +1,31 @@
-with old_tx as (
+with
+    old_tx as (
 
-    select
-        transaction_date as date
-        , amount
-        , sum(amount) over (order by transaction_date) as balance
-    from {{ ref('hledger_transactions') }}
-    where account = 'Assets:Investments:cosmosdirect BR'
+        select
+            transaction_date as date,
+            amount,
+            sum(amount) over (order by transaction_date) as balance
+        from {{ ref("hledger_transactions") }}
+        where account = 'Assets:Investments:cosmosdirect BR'
 
-)
-
-, new_tx as (
-    select
-       date
-       , balance
-    from {{ ref('cosmos_balance') }}
-)
-
-, all_tx as (
-    select
-       date
-       , balance
-    from old_tx
-    union all
-    select
-      date
-      , balance
-    from new_tx
-)
+    ),
+    new_tx as (select date, balance from {{ ref("cosmos_balance") }}),
+    all_tx as (
+        select date, balance
+        from old_tx
+        union all
+        select date, balance
+        from new_tx
+    )
 
 select
-   date as transaction_date
-   , balance
-   -- running difference
-   , balance - lag(balance, 1, 0) over (order by date) as amount_eur
-   , 'Assets:Investments:cosmosdirect BR' as account
-   , 'CosmosDirect wrangling' as description
-   , 'VirtualIncome:Investment Transfers' as category
-   -- generate tx id
-    , md5(date::text || balance::text) as transaction_id
+    date as transaction_date,
+    balance,
+    -- running difference
+    balance - lag(balance, 1, 0) over (order by date) as amount_eur,
+    'Assets:Investments:cosmosdirect BR' as account,
+    'CosmosDirect wrangling' as description,
+    'VirtualIncome:Investment Transfers' as category,
+    -- generate tx id
+    md5(date::text || balance::text) as transaction_id
 from all_tx
