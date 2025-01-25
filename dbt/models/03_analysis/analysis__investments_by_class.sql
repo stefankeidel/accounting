8de2217caf311df580f0b7d1872a8fce1db05521
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2025-01-25 13:39:06 +0100

    set up for crypto eh

--- dbt/models/03_analysis/analysis__investments_by_class.sql
+++ dbt/models/03_analysis/analysis__investments_by_class.sql
@@ -20,23 +20,24 @@ select
     snapshot_month,
     equity,
     bond,
-    equity + bond as total,
-    100.0 * equity / (equity + bond) as equity_percentage,
-    100.0 * bond / (equity + bond) as bond_percentage,
+    crypto,
+    equity + bond + crypto as total,
+    100.0 * equity / (equity + bond + crypto) as equity_percentage,
+    100.0 * bond / (equity + bond + crypto) as bond_percentage,
     equity - lag(equity, 1, equity) over (order by snapshot_month) as equity_diff,
     bond - lag(bond, 1, bond) over (order by snapshot_month) as bond_diff,
     equity
     + bond
-    - lag(equity + bond, 1, equity + bond) over (order by snapshot_month) as total_diff,
-    100.0 * equity / (equity + bond)
-    - lag(100.0 * equity / (equity + bond), 1, 100.0 * equity / (equity + bond)) over (
+    - lag(equity + bond + crypto, 1, equity + bond + crypto) over (order by snapshot_month) as total_diff,
+    100.0 * equity / (equity + bond + crypto)
+    - lag(100.0 * equity / (equity + bond + crypto), 1, 100.0 * equity / (equity + bond + crypto)) over (
         order by snapshot_month
     ) as equity_percentage_diff,
-    100.0 * bond / (equity + bond)
-    - lag(100.0 * bond / (equity + bond), 1, 100.0 * bond / (equity + bond)) over (
+    100.0 * bond / (equity + bond + crypto)
+    - lag(100.0 * bond / (equity + bond + crypto), 1, 100.0 * bond / (equity + bond + crypto)) over (
         order by snapshot_month
     ) as bond_percentage_diff,
     100.0
-    * (equity + bond - lag(equity + bond) over (order by snapshot_month))
-    / lag(equity + bond) over (order by snapshot_month) as percentage_change
+    * (equity + bond + crypto - lag(equity + bond + crypto) over (order by snapshot_month))
+    / lag(equity + bond + crypto) over (order by snapshot_month) as percentage_change
 from pivot
